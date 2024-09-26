3d24bd7a03656e9f6f9de83f556d832185826c02
Damian Jerez <jerezdamian640@gmail.com>
2024-09-26 09:51:49 -0400

    completed shoppingList app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        //print hello world to console/terminal
+        System.out.println("hello world");
+    }
+}
+
