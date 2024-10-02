0eb2cef1bc9ccab8bab8b99bb8d5f5db43628680
Damian <jerezdamian640@gmail.com>
2024-10-02 00:39:05 -0400

    updated main

---
+++ workbook-1/financial-calculator/src/main/java/com/pluralsight/ColorConsole.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class ColorConsole {
+
+    public static void main(String[] args) {
+        //reset
+        String reset = "\033[0m";
+        String red = "\033[0;31";
+        String cyan = "\033[0;36m";
+    }
+}
--- workbook-1/financial-calculator/src/main/java/com/pluralsight/main.java
+++ workbook-1/financial-calculator/src/main/java/com/pluralsight/main.java
@@ -6,17 +6,22 @@ import java.util.Scanner;
 
 public class main {
 
-    public static void main(String[] args) {  //called classes
+    public static <string> void main(String[] args) {  //called classes
         FutureValue fv = new FutureValue();
         Mortgage m = new Mortgage();
         Annuity A = new Annuity();
+        String reset = "\033[0m";
+        String red = "\033[0;31m";
+        String cyan = "\033[0;36m";
+        String magenta = "\033[0;35m";
 
         Scanner logicscanner = new Scanner(System.in); //made scanner
 
         boolean loop = true;  // conditional loop variable
         while (loop) {
-            System.out.println("\n***SELECT YOUR CALCULATOR***\n");
-            System.out.println("For future value calculator select 1\n\nFor mortgage calculator select 2\n\nFor annuity calculator select 3\n\n To exit select 4 ");
+            System.out.println( cyan + "\n***SELECT YOUR CALCULATOR***\n" + reset);
+            System.out.println(magenta + "For future value calculator select 1\n\nFor mortgage calculator select 2\n\nFor annuity calculator select 3\n\n" + reset);
+            System.out.println(red + "To exit select 4 " +reset);
             int userChoice = logicscanner.nextInt();
             if (userChoice == 1) {
                 fv.FutureValue();
@@ -27,7 +32,7 @@ public class main {
             }
             else if (userChoice == 4) {
                 loop = false;
-                System.out.println("SCREW YOU THEN!!");
+                System.out.println( "SCREW YOU THEN!!");
             }
             else{System.out.println("Would you like to exit this program?\nEnter 1 for yes\nEnter 2 for no");
             userChoice = logicscanner.nextInt();  // made it loop if user puts incorrect input
